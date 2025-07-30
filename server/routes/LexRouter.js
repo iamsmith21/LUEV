120db5d6fd30cd301c3995578a8c869c2f3402bd
Smith <deep.smith100@gmail.com>
2025-07-30 17:41:43 -0400

    Added LexRouter and Controller for Lex Chatbot

--- server/routes/LexRouter.js
+++ server/routes/LexRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.router();
+const router = express.Router();
 const {getCarByName, compareEVs} = require('../controller/LexController');
 
 router.post('/lex-webhook', async (req, res) => {
