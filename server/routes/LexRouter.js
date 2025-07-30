cf8db2bd1e7ec311eb038c97d0b56cd98ba9a58f
Smith <deep.smith100@gmail.com>
2025-07-30 17:29:39 -0400

    Added LexRouter and Controller for Lex Chatbot

---
+++ server/routes/LexRouter.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.router();
+const {getCarByName, compareEVs} = require('../controller/LexController');
+
+router.post('/lex-webhook', async (req, res) => {
+    const {intent} = req.body;
+
+    if (intent === 'SearchCarByName') {
+    const msg = await getCarByName(req.body.model_name);
+    return res.json({ message: msg });
+  }
+
+  if (intent === 'CompareEVs') {
+    const msg = await compareEVs(req.body.car_one, req.body.car_two);
+    return res.json({ message: msg });
+  }
+
+//   if (intent === 'SearchCarByFeature') {
+//     const msg = await searchByFeature(req.body.feature_type);
+//     return res.json({ message: msg });
+//   }
+
+  return res.json({ message: "Invalid intent." });
+
+});
+
+module.exports = router;
